5160ef74c0606b7c4ca22b0197b9667b7d81fd4e
Cong Luan Tran <congluan.tran19@gmail.com>
2025-05-30 10:47:55 +0700

    feat: add the response header class

---
+++ app/response.py
@@ -0,0 +1,19 @@
+class ResponseHeader:
+    def __init__(self, correlation_id: int):
+        self.correlation_id = correlation_id
+
+    def size(self) -> bytes:
+        header_size = len(bytes(self))
+        return header_size.to_bytes(4, byteorder="big", signed=True)
+
+    @property
+    def correlation_id(self):
+        """The correlation_id property."""
+        return self._correlation_id
+
+    @correlation_id.setter
+    def correlation_id(self, value):
+        self._correlation_id = value
+
+    def __bytes__(self) -> bytes:
+        return self.correlation_id.to_bytes(4, byteorder="big", signed=True)
