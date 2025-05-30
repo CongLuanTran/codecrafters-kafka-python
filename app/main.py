fc3294346b6dc31b42dcb5e40743d68f604881a5
Cong Luan Tran <congluan.tran19@gmail.com>
2025-05-30 10:48:18 +0700

    feat: send message size and response header to the client

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 import socket  # noqa: F401
+from app.response import ResponseHeader
 
 
 def main():
     print("Logs from your program will appear here!")
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept()  # wait for client
+    conn, addr = server.accept()  # wait for client
+    header = ResponseHeader(7)
+    conn.sendall(header.size())
+    conn.sendall(bytes(header))
 
 
 if __name__ == "__main__":
