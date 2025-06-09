a9fe80bcd0c0a71e0b2555e1525dd9e4bbe69f9b
Cong Luan Tran <congluan.tran19@gmail.com>
2025-06-09 15:26:41 +0700

    fix: revert back to check import issue

--- app/main.py
+++ app/main.py
@@ -1,26 +1,25 @@
 import socket  # noqa: F401
-from api_versions import ApiVersionsResponse
-from request import KafkaRequest
-from response import KafkaResponse, KafkaResponseHeader
+from app.api_versions import ApiVersionsResponse
+from app.request import KafkaRequest
+from app.response import KafkaResponse, KafkaResponseHeader
 
 
 def main():
     print("Logs from your program will appear here!")
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     conn, addr = server.accept()  # wait for client
-    while True:
-        message_size = conn.recv(4)
-        message_size = int.from_bytes(message_size, "big", signed=True)
+    message_size = conn.recv(4)
+    message_size = int.from_bytes(message_size, "big", signed=True)
 
-        message = conn.recv(message_size)
-        request = KafkaRequest.deserialize(iter(message))
+    message = conn.recv(message_size)
+    request = KafkaRequest.deserialize(iter(message))
 
-        response_header = KafkaResponseHeader(request.header.correlation_id)
-        response_body = None
-        if request.header.api_key == 18:
-            response_body = ApiVersionsResponse(request.header.api_version)
-        response = KafkaResponse(response_header, response_body)
-        conn.sendall(bytes(response.size()) + bytes(response))
+    response_header = KafkaResponseHeader(request.header.correlation_id)
+    response_body = None
+    if request.header.api_key == 18:
+        response_body = ApiVersionsResponse(request.header.api_version)
+    response = KafkaResponse(response_header, response_body)
+    conn.sendall(bytes(response.size()) + bytes(response))
 
 
 if __name__ == "__main__":
